9145f63f3195e325bf7282df23288499018883b4
AnnaPalarkina171 <annettepalatkina@gmail.com>
2017-11-06 19:16:28 +0300

    hw3

---
+++ hw3/HW.py
@@ -0,0 +1,12 @@
+n = int(input('Введите число: '))
+i = 2
+while i <= n:
+    if n == False:
+        break
+    if n == 2:
+        print(i)
+        i *= 2
+    else:
+        print(i)
+        i *= 2
+

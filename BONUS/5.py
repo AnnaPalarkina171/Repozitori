d6b7fa24eaade7d3bec7817de1d84c11c65fec77
AnnaPalarkina171 <31884615+AnnaPalarkina171@users.noreply.github.com>
2018-02-02 19:54:58 +0300

    Add files via upload

---
+++ BONUS/5.py
@@ -0,0 +1,23 @@
+n = input('Enter sth: ')
+alf = 'qwrtpsdfghjklzxcvbnm'
+for i in n:
+    if i in alf:
+        i = i + 'aig' 
+        print(i, end='')
+    else:
+        print(i, end='')
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+  

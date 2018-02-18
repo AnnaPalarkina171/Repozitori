322cfe316477e6ec69d9a13be7f59ea820f51060
AnnaPalarkina171 <31884615+AnnaPalarkina171@users.noreply.github.com>
2018-02-18 21:13:03 +0300

    hw

--- hw8/1.py
+++ hw8/1.py
@@ -1,40 +1,47 @@
 import random
-with open('text.txt',encoding='utf-8')as t:
-    text = t.read()
-    text = text.splitlines()
-    print('Вставьте вместо многоточия существительное')
-    d = {}
-    l=[]
-    s = []
-    i = 1
+def ex():
+    with open('text.txt',encoding='utf-8')as t:
+        text = t.read()
+        text = text.splitlines()
+        print('Вставьте вместо многоточия существительное')
+        d = {}
+        l = []
+        s = []
+        i = 1
 
-with open('table - table.csv', encoding = 'utf-8') as f:
-    for line in f:
-        w = line.split(',')
-        value = w[1:]
-        value[len(value)-1] = value[len(value)-1].replace('\n','')
-        value[len(value)-1] = value[len(value)-1].replace('"','')
-        w[0] = w[0].replace('"','')
-        key = w[0]
-        l.append(key)
-        keys = d.setdefault(key,value)
-        k = list(d.keys())
-        r = random.choice(k)
-    if r in d.keys():
-        hint = d[r]
+        
+    with open('table - table.csv', encoding = 'utf-8') as f:
+        for line in f:
+            w = line.split(',')
+            value = w[1:]
+            value[len(value)-1] = value[len(value)-1].replace('\n','')
+            value[len(value)-1] = value[len(value)-1].replace('"','')
+            w[0] = w[0].replace('"','')
+            key = w[0]
+            l.append(key)
+            keys = d.setdefault(key,value)
+            k = list(d.keys())
+            r = random.choice(k)
+        if r in d.keys():
+            hint = d[r]
 
-while s == []:
-        if len(hint) == i:
-                print('К сожалению, Вы использовали все попытки')
-                break
-        print(hint[i])
-        n = input('Слово: ')
-        if r == n:
-            print(random.choice(text))
-            s.append(r)
-        if r != n:
-            print('Увы, нет. Вот Вам ещё одна подсказка: ')
-            i += 1
+    while s == []:
+            if len(hint) == i:
+                    print('К сожалению, Вы использовали все попытки')
+                    break
+            print(hint[i])
+            n = input('Слово: ')
+            if r == n:
+                s.append(r)
+                print(random.choice(text))
+                print('Давайте сыграем ещё раз!')
+                print(ex())
+            if r != n:
+                print('Увы, нет. Вот Вам ещё одна подсказка: ')
+                i += 1            
+print(ex())
+
+
         
         
             

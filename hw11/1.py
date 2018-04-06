75776a35ce4363669550133cd1ebbde1b88c41f2
AnnaPalarkina171 <31884615+AnnaPalarkina171@users.noreply.github.com>
2018-04-06 20:36:46 +0300

    hw

---
+++ hw11/1.py
@@ -0,0 +1,30 @@
+import re
+
+with open('birds.html', encoding='utf-8') as f:
+    text = f.read()
+    text = re.sub('птица','рыба',text)
+    text = re.sub('птицы','рыбы',text)
+    text = re.sub('птиц','рыб',text)
+    text = re.sub('птице','рыбе',text)
+    text = re.sub('птицам','рыбам',text)
+    text = re.sub('птицу','рыбу',text)
+    text = re.sub('птицей','рыбой',text)
+    text = re.sub('птицею','рыбою',text)
+    text = re.sub('птицами','рыбами',text)
+    text = re.sub('птицах','рыбах',text)
+
+    text = re.sub('Птица','Рыба',text)
+    text = re.sub('Птицы','Рыбы',text)
+    text = re.sub('Птиц','Рыб',text)
+    text = re.sub('Птице','Рыбе',text)
+    text = re.sub('Птицам','Рыбам',text)
+    text = re.sub('Птицу','Рыбу',text)
+    text = re.sub('Птицей','Рыбой',text)
+    text = re.sub('Птицею','Рыбою',text)
+    text = re.sub('Птицами','Рыбами',text)
+    text = re.sub('Птицах','Рыбах',text)
+
+    with open('fish.txt','w',encoding='utf-8') as f:
+        f.write(text)
+
+#очень тупо, но я не знаю, что делать, чтобы регистр первых букв оставался
